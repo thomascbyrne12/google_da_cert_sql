f3e210e2c8293978a3702bdf39cbb5228a6d3687
Thomas Byrne <thomascbyrne563@gmail.com>
2021-06-22 16:46:22 -0400

    Editing

--- data_transformation.sql
+++ data_transformation.sql
@@ -30,3 +30,15 @@ SELECT
     COALESCE(product, product_code) AS product_info
 FROM
     customer_data.customer_purchase;
+
+
+SELECT
+    customer_id,
+    CASE
+        WHEN first_name = 'Tnoy' THEN 'Tony'
+        WHEN first_name = 'Tmo' THEN 'Tom'
+        WHEN first_name = 'Rachle' THEN 'Rachel'
+        ELSE first_name
+        END AS cleaned_name
+FROM
+    customer_data.customer_name;
