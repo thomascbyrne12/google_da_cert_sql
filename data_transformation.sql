026f01bf6975527bc4862feeb5766ffa282286a8
Thomas Byrne <thomascbyrne563@gmail.com>
2021-06-22 16:25:39 -0400

    New

---
+++ data_transformation.sql
@@ -0,0 +1,32 @@
+/*Return all purchase prices in descending order
+    as float variables */
+SELECT
+    CAST(purchase_price AS FLOAT)
+FROM
+    customer_data.customer_purchase
+ORDER BY
+    CAST(purchase_price AS FLOAT) DESC;
+
+--Returns table of purchases in December 2020
+SELECT
+    CAST(date AS DATE) AS DATE only,
+    purchase_price
+FROM
+    customer_data.customer_purchase
+WHERE
+    date BETWEEN '2020-12-01' AND '2020-12-31';
+
+/*Returns list of color and product code data
+    for couch purchases */
+SELECT
+    CONCAT(product_code, product_color) AS new_product_code
+FROM
+    customer_data.customer_purchase
+WHERE
+    product = 'couch'
+
+--Returns list of products sold
+SELECT
+    COALESCE(product, product_code) AS product_info
+FROM
+    customer_data.customer_purchase;
